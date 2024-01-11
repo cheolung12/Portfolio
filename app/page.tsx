3996e2c9b84b8f38c378ba5971156ed4b0190483
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2024-01-11 09:45:29 +0900

    introduction component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Image from 'next/image'
+import About from '@/components/about'
+import Intro from '@/components/intro'
+import SectionDivider from '@/components/section-divider'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <Intro />
+      <SectionDivider />
+      <About />
+      <SectionDivider />
     </main>
   )
 }
