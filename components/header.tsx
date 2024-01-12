9ba44170c78b9f458be61a14279fd79975b08ad4
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2024-01-13 04:38:12 +0900

    header observer, animation

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,16 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import { motion } from 'framer-motion';
 import { links } from '@/lib/data';
 import Link from 'next/link';
 import clsx from 'clsx';
+import { useActiveSectionContext } from '@/context/active-section-context';
 
 export default function Header() {
-  const [activateSection, setActivateSection] = useState('Home');
+  const { activeSection, setActiveSection, setTimeOfLastClick } =
+    useActiveSectionContext();
+  
   return (
     <header className='z-[999] relative'>
       <motion.div
@@ -25,10 +28,33 @@ export default function Header() {
               animate={{ y: 0, opacity: 1 }}
             >
               <Link
+                className={clsx(
+                  'flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300',
+                  {
+                    'text-gray-950 dark:text-gray-200':
+                      activeSection === link.name,
+                  }
+                )}
                 href={link.hash}
-                className={clsx('flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300')}
+                onClick={() => {
+                  setActiveSection(link.name);
+                  setTimeOfLastClick(Date.now());
+                }}
               >
                 {link.name}
+
+                {link.name === activeSection && (
+                  <motion.span
+                    className='bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800'
+                    // 공유 레이아웃 애니메이션
+                    layoutId='activeSection'
+                    transition={{
+                      type: 'spring',
+                      stiffness: 380,
+                      damping: 30,
+                    }}
+                  ></motion.span>
+                )}
               </Link>
             </motion.li>
           ))}
---
+++ lib/hooks.ts
@@ -0,0 +1,22 @@
+import { useActiveSectionContext } from "@/context/active-section-context";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import type { SectionName } from "./types";
+
+export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
+  // 해당 컴포넌트가 viewport 안에 있다면 inView가 true
+  const { ref, inView } = useInView({
+    threshold,
+  });
+  const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
+      setActiveSection(sectionName);
+    }
+  }, [inView, setActiveSection, timeOfLastClick, sectionName]);
+
+  return {
+    ref,
+  };
+}
