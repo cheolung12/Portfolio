20d11ecec72702d5c0da441bc8cf8f959f8c96be
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2024-01-15 13:22:49 +0900

    experience component

--- lib/data.ts
+++ lib/data.ts
@@ -69,3 +69,30 @@ export const skillsData = [
   "Spring",
   "AWS",
 ] as const;
+
+export const experiencesData = [
+  {
+    title: "Graduated bootcamp",
+    location: "Miami, FL",
+    description:
+      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+    icon: React.createElement(LuGraduationCap),  // tsx가 아닌 ts 파일이므로 <LuGraduationCap /> 이렇게 안하고 createElement함수 사용
+    date: "2019",
+  },
+  {
+    title: "Front-End Developer",
+    location: "Orlando, FL",
+    description:
+      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2019 - 2021",
+  },
+  {
+    title: "Full-Stack Developer",
+    location: "Houston, TX",
+    description:
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+    icon: React.createElement(FaReact),
+    date: "2021 - present",
+  },
+] as const;
