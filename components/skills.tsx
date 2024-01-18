0872b0567f9fd38bb66970de9d6a38a740b8f469
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2024-01-18 22:16:52 +0900

    스킬 컴포넌트에 아이콘 추가

--- components/skills.tsx
+++ components/skills.tsx
@@ -24,24 +24,32 @@ export default function Skills() {
 
   return (
     <section
-      id="skills"
+      id='skills'
       ref={ref}
       className='mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40'
     >
-      <SectionHeading>My skills</SectionHeading>
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <SectionHeading>My skills</SectionHeading>
+      </motion.div>
       <ul className='flex flex-wrap justify-center gap-2 text-lg text-gray-800'>
         {skillsData.map((skill, idx) => (
           <motion.li
             key={idx}
-            initial="initial"
-            whileInView="animate"
+            initial='initial'
+            whileInView='animate'
             variants={fadeInAnimation}
             viewport={{
-              once: true
+              once: true,
             }}
             className='bg-white border border-black/[0.1] rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80'
           >
-            {skill}
+            <span className='flex justify-center items-center gap-1'>
+              {' '}
+              {skill.name} {skill.icon}{' '}
+            </span>
           </motion.li>
         ))}
       </ul>
--- lib/data.ts
+++ lib/data.ts
@@ -1,94 +1,211 @@
-import React from "react";
-import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
-import syosImg from "@/public/syos.png";
-import rmtdevImg from "@/public/rmtdev.png";
+import React from 'react';
+import { CgWorkAlt } from 'react-icons/cg';
+import {
+  FaReact,
+  FaNodeJs,
+  FaBootstrap,
+  FaJava,
+  FaAws,
+  FaLaptopCode,
+} from 'react-icons/fa';
+import { LuGraduationCap } from 'react-icons/lu';
+import { AiOutlineHtml5 } from 'react-icons/ai';
+import { RiCss3Line } from 'react-icons/ri';
+import { IoLogoJavascript } from 'react-icons/io5';
+import {
+  SiTypescript,
+  SiNextdotjs,
+  SiTailwindcss,
+  SiRedux,
+  SiSequelize,
+  SiSocketdotio,
+  SiExpress,
+  SiSpringboot,
+} from 'react-icons/si';
+import { GrMysql } from 'react-icons/gr';
+import syosImg from '@/public/syos.png';
+import appoImg from '@/public/appointment.png';
+import bluejeansImg from '@/public/bluejeans.png';
 
 export const links = [
   {
-    name: "Home",
-    hash: "#home",
+    name: 'Home',
+    hash: '#home',
   },
   {
-    name: "Skills",
-    hash: "#skills",
+    name: 'Skills',
+    hash: '#skills',
   },
   {
-    name: "Projects",
-    hash: "#projects",
+    name: 'Projects',
+    hash: '#projects',
   },
   {
-    name: "Experience",
-    hash: "#experience",
+    name: 'Experience',
+    hash: '#experience',
   },
   {
-    name: "Contact",
-    hash: "#contact",
+    name: 'Contact',
+    hash: '#contact',
   },
 ] as const;
 
-
 export const projectsData = [
   {
-    title: "SYOS",
+    title: 'SYOS',
+    subtitle: '데스크탑 공유 & 추천 웹 서비스',
+    roles: ['메인페이지 디자인 및 구현', '채팅', '게시물 상세'],
     description:
-      "데스크탑 공유 & 추천 웹 서비스",
-    tags: ["HTML", "CSS", "JavaScript", "Node.js", "express", "MySQL", "Sequelize"],
+      '첫 웹 풀스택 프로젝트입니다. 추후 백엔드와의 원할환 의사소통을 위하여 DB까지 직접 만져보았고, RDS, S3도 활용하였습니다. 또한 순수 CSS로 스타일링하여 기초적인 CSS를 잘 다루게된 계기가 되었습니다.',
+    tags: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'Node.js',
+      'express',
+      'MySQL',
+      'Sequelize',
+    ],
     imageUrl: syosImg,
+    githubLink: 'https://github.com/cheolung12/SYOS',
+    postLink: [],
   },
   {
-    title: "rmtDev",
+    title: '내일어때?',
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+      'Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.',
+    tags: ['React', 'Javascript', 'Tailwind', 'Redux', 'Kakao Map API'],
+    imageUrl: appoImg,
+    githubLink: 'https://github.com/cheolung12/appointment-making-project',
+    postLink: [],
+  },
+  {
+    title: 'BlueJeans',
+    description:
+      'Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.',
+    tags: [
+      'React',
+      'Javascript',
+      'Tailwind',
+      'Open AI API',
+      'react-speech-recognition',
+      'SpringBoot',
+      'JPA',
+      'MySQL',
+    ],
+    imageUrl: bluejeansImg,
+    githubLink: 'https://github.com/cheolung12/BlueJeans',
+    postLink: [],
+  },
+  {
+    title: 'Portfolio',
+    description:
+      'Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.',
+    tags: [
+      'React',
+      'Typescript',
+      'Next.js',
+      'Tailwind',
+      'Framer-motion',
+      'resend',
+    ],
+    imageUrl: bluejeansImg,
+    githubLink: 'https://github.com/cheolung12/portfolio',
+    postLink: [],
   },
   // ...
 ] as const;
 
 export const skillsData = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "TypeScript",
-  "React",
-  "Next.js",
-  "Node.js",
-  "Tailwind",
-  "Redux",
-  "MySQL",
-  "Sequelize",
-  "Socket IO",
-  "Express",
-  "Java",
-  "Spring",
-  "AWS",
+  {
+    name: 'HTML',
+    icon: React.createElement(AiOutlineHtml5),
+  },
+  {
+    name: 'CSS',
+    icon: React.createElement(RiCss3Line),
+  },
+  {
+    name: 'JavaScript',
+    icon: React.createElement(IoLogoJavascript),
+  },
+  {
+    name: 'TypeScript',
+    icon: React.createElement(SiTypescript),
+  },
+  {
+    name: 'React',
+    icon: React.createElement(FaReact),
+  },
+  {
+    name: 'Next.js',
+    icon: React.createElement(SiNextdotjs),
+  },
+  {
+    name: 'Node.js',
+    icon: React.createElement(FaNodeJs),
+  },
+  {
+    name: 'TailwindCSS',
+    icon: React.createElement(SiTailwindcss),
+  },
+  {
+    name: 'Redux',
+    icon: React.createElement(SiRedux),
+  },
+  {
+    name: 'Bootstrap',
+    icon: React.createElement(FaBootstrap),
+  },
+  {
+    name: 'MySQL',
+    icon: React.createElement(GrMysql),
+  },
+  {
+    name: 'Sequelize',
+    icon: React.createElement(SiSequelize),
+  },
+  {
+    name: 'Socket IO',
+    icon: React.createElement(SiSocketdotio),
+  },
+  {
+    name: 'Express',
+    icon: React.createElement(SiExpress),
+  },
+  {
+    name: 'Java',
+    icon: React.createElement(FaJava),
+  },
+  {
+    name: 'SpringBoot',
+    icon: React.createElement(SiSpringboot),
+  },
+  {
+    name: 'AWS',
+    icon: React.createElement(FaAws),
+  },
 ] as const;
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: '국민대학교 소프트웨어학부',
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-    icon: React.createElement(LuGraduationCap),  // tsx가 아닌 ts 파일이므로 <LuGraduationCap /> 이렇게 안하고 createElement함수 사용
-    date: "2019",
+      'I graduated after 6 months of studying. I immediately found a job as a front-end developer.',
+    icon: React.createElement(LuGraduationCap), // tsx가 아닌 ts 파일이므로 <LuGraduationCap /> 이렇게 안하고 createElement함수 사용
+    date: '- 2023.08',
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: '포스코 x 코딩온 웹 풀스택 부트캠프',
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      'I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.',
+    icon: React.createElement(FaLaptopCode),
+    date: '2019 - 2021',
+  },
+  {
+    title: '...',
+    description: '...',
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
-  },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: 'present',
   },
 ] as const;
