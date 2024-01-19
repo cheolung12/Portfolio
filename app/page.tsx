8543d81a4225b1f1dc72ff965ad43191f17f62a8
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2024-01-19 09:03:00 +0900

    fix 사용하지 않는 import 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import About from '@/components/about';
 import Contact from '@/components/contact';
 import Experience from '@/components/experience';
 import Intro from '@/components/intro';
